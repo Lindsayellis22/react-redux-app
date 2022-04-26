6d446ceee5c1f84d956deb075074ce3390e00582
lindsayellis22 <lindsayellis39@gmail.com>
2022-04-26 15:05:01 -0400

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Logo from "/Users/lindsayellis/project/react-redux-project/src/assets/chicken.ico";
 
 function Header(props) {
-  return <div className="fixed z-50 w-screen bg-slate-300 p-6"></div>;
+  return (
+    <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
+      <div className="hidden md:flex w-full h-full">
+        <div className="flex items-center gap-2">
+          <img src={Logo} className="w-10 object-cover" alt="logo" />
+        </div>
+      </div>
+
+      <div className="fex md:hidden w-full h-full"></div>
+    </div>
+  );
 }
 
 export default Header;
